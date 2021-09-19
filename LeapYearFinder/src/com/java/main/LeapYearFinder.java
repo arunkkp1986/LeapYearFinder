5715f06a141efa9c443e60529f3f728a3aefd7ac
Arunkumar <arunkkp1986@gmail.com>
2021-09-20 01:48:54 +0800

    Write code to return true is given year value is leap year else should return false

--- LeapYearFinder/src/com/java/main/LeapYearFinder.java
+++ LeapYearFinder/src/com/java/main/LeapYearFinder.java
@@ -2,8 +2,12 @@ package com.java.main;
 
 public class LeapYearFinder {
 
-	public boolean isLeapYear(int i) {
-		return false;
+	public boolean isLeapYear(int year) {
+		boolean isLeapYear = false;
+		if((year % 4 == 0 && year %100 != 0) || year %400 == 0) {
+			isLeapYear = true;
+		}
+		return isLeapYear;
 	}
 
 }
--- LeapYearFinder/src/com/test/main/LeapYearFinderTest.java
+++ LeapYearFinder/src/com/test/main/LeapYearFinderTest.java
@@ -7,17 +7,12 @@ import org.junit.jupiter.api.Test;
 import com.java.main.LeapYearFinder;
 
 class LeapYearFinderTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 	
 	@Test
 	void testLeapYear() {
 		LeapYearFinder leapYearFinder = new LeapYearFinder();
 		boolean expected = true;
-		boolean actual = leapYearFinder.isLeapYear(2003);
+		boolean actual = leapYearFinder.isLeapYear(2004);
 		assertEquals(expected, actual,"The isLeapYear method should return leap year or not");
 	}
 
